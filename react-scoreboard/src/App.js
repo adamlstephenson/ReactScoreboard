d917ffc60b8db17cfdefd65a6f8119703f9715a9
Adam Stephenson <astephensonx@gmail.com>
2018-06-28 22:03:53 -0500

    basic scoreboard outline complete

--- react-scoreboard/src/App.js
+++ react-scoreboard/src/App.js
@@ -7,12 +7,34 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Scoreboard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+              Adam Stephenson
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="decrement"> - </button>
+                <div className="counter-score"> 29 </div>
+                <button className="increment"> + </button>
+              </div>
+            </div>
+          </div>
+          <div className="player">
+            <div className="player-name">
+              Erik Stephenson
+            </div>
+            <div className="player-score">
+              <div className="counter">
+                <button className="decrement"> - </button>
+                <div className="counter-score"> 2 </div>
+                <button className="increment"> + </button>
+              </div>
+            </div>
+          </div>
+        </div >
       </div>
     );
   }
